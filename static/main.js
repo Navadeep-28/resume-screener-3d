4c463d4bdf2232c9cdf4b554257fbb434e79cff5
Navadeep-28 <navadeep285@gmail.com>
2026-01-16 16:37:05 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,9 +1,5 @@
-import { initBatchScreening } from "./batch.js";
-import { initResumeComparison } from "./compare.js";
 import { initModeSwitch } from "./mode-switch.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    initBatchScreening();
-    initResumeComparison();
     initModeSwitch();
 });
